320d889bf1168866304df55e3e0e5431aa0e9b69
JaymeMorrissey99 <jaymemorrissey99@gmail.com>
2022-04-25 00:49:33 +0100

    bottom nav in customer

--- app/src/main/java/com/example/assignment4/AdminMain.java
+++ app/src/main/java/com/example/assignment4/AdminMain.java
@@ -18,32 +18,5 @@ public class AdminMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_menu);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFrag= null;
-                switch (item.getItemId()){
-                    case R.id.home:
-                        Toast.makeText(getApplicationContext(), "Home Selected", Toast.LENGTH_SHORT).show();
-//                        selectedFrag = new CompanyHomeFragment();
-                        break;
-                    case R.id.search:
-                        Toast.makeText(getApplicationContext(), "Search Selected", Toast.LENGTH_SHORT).show();
-//                        selectedFrag = new SearchFragment();
-                        break;
-                    case R.id.addPost:
-                        selectedFrag=null;
-                        //Toast.makeText(getApplicationContext(), "Post Selected", Toast.LENGTH_SHORT).show();
-                        //SendToPost();
-                        break;
-//                    default:
-//                        selectedFrag = new CompanyHomeFragment();
-
-                }
-//                getSupportFragmentManager().beginTransaction().replace(R.id.cfrag_containter, selectedFrag).commit();
-                return true;
-            }
-        });
     }
 }
--- app/src/main/java/com/example/assignment4/MainActivity.java
+++ app/src/main/java/com/example/assignment4/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.assignment4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +33,31 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_menu);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFrag= null;
+                switch (item.getItemId()){
+                    case R.id.shop:
+                        Toast.makeText(getApplicationContext(), "Shop Selected", Toast.LENGTH_SHORT).show();
+//                        selectedFrag = new CompanyHomeFragment();
+                        break;
+                    case R.id.mystuff:
+                        Toast.makeText(getApplicationContext(), "My Stuff Selected", Toast.LENGTH_SHORT).show();
+//                        selectedFrag = new SearchFragment();
+                        break;
+                        //Toast.makeText(getApplicationContext(), "Post Selected", Toast.LENGTH_SHORT).show();
+                        //SendToPost();
+//                    default:
+//                        selectedFrag = new CompanyHomeFragment();
+
+                }
+//                getSupportFragmentManager().beginTransaction().replace(R.id.cfrag_containter, selectedFrag).commit();
+                return true;
+            }
+        });
 
     }
 }
