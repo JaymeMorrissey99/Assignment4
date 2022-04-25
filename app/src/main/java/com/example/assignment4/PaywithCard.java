bf095ae7e437693b84a111de3c4072086640e5e9
JaymeMorrissey99 <jaymemorrissey99@gmail.com>
2022-04-26 00:28:33 +0100

    working on stock updates when users pays

--- app/src/main/java/com/example/assignment4/PaywithCard.java
+++ app/src/main/java/com/example/assignment4/PaywithCard.java
@@ -3,6 +3,8 @@ package com.example.assignment4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,9 +21,34 @@ public class PaywithCard extends AppCompatActivity {
     TextView cardName, cardNumber, cardEx;
     EditText c;
 
+    Button payBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paywith_card);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        cardName = findViewById(R.id.usercardName);
+        cardNumber = findViewById(R.id.usercardNumber);
+        cardEx = findViewById(R.id.userexpireDate);
+        c = findViewById(R.id.usercvc);
+        payBtn = findViewById(R.id.paybtn);
+
+        payBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        LoadData();
+
+
+
+    }
+
+    private void LoadData() {
+
     }
 }
