205c835114f99848420c33b4f0b1b1043e165d8b
JaymeMorrissey99 <jaymemorrissey99@gmail.com>
2022-04-25 01:12:17 +0100

    Admin main navigation

---
+++ app/src/main/java/com/example/assignment4/AdminCustomerDetails.java
@@ -0,0 +1,14 @@
+package com.example.assignment4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminCustomerDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_customer_details);
+    }
+}
--- app/src/main/java/com/example/assignment4/AdminMain.java
+++ app/src/main/java/com/example/assignment4/AdminMain.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -20,6 +22,40 @@ public class AdminMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+
+        stockbtn = findViewById(R.id.stockBtn);
+        customerdbtn = findViewById(R.id.cusBtn);
+        purchase = findViewById(R.id.pHistory);
+
+
+        stockbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminMain.this, "Successful Register", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(AdminMain.this, AdminStock.class);
+                startActivity(intent);
+            }
+        });
+
+        customerdbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminMain.this, "Successful Register", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(AdminMain.this, AdminCustomerDetails.class);
+                startActivity(intent);
+            }
+        });
+
+        purchase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminMain.this, "Successful Register", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(AdminMain.this, AdminPurchaseHistory.class);
+                startActivity(intent);
+
+            }
+        });
+
 
     }
 }
