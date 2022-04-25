9c26712fb792c2ea2c00af8da7f080167be141b4
JaymeMorrissey99 <jaymemorrissey99@gmail.com>
2022-04-26 00:39:58 +0100

    loading card data and cvc check done

--- app/src/main/java/com/example/assignment4/PaywithCard.java
+++ app/src/main/java/com/example/assignment4/PaywithCard.java
@@ -1,5 +1,6 @@
 package com.example.assignment4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,16 +8,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PaywithCard extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseUser mUser;
-    FirebaseDatabase cardRef;
+    DatabaseReference cardRef;
+    String cname, cnum, cex, cvc;
 
     TextView cardName, cardNumber, cardEx;
     EditText c;
@@ -35,10 +42,12 @@ public class PaywithCard extends AppCompatActivity {
         c = findViewById(R.id.usercvc);
         payBtn = findViewById(R.id.paybtn);
 
+        cardRef = FirebaseDatabase.getInstance().getReference().child("Cardpayments");
+
         payBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                pay();
             }
         });
 
@@ -48,7 +57,83 @@ public class PaywithCard extends AppCompatActivity {
 
     }
 
+    private void pay() {
+        String inputcvc = c.getText().toString().trim();
+        if(inputcvc.equalsIgnoreCase(cvc)){
+            Toast.makeText(this, "Correct Pin", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Wrong PiN\nPlease Try Again", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void LoadData() {
 
+        cardRef.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    cname = snapshot.child("cardName").getValue().toString();
+                    cnum = snapshot.child("cardNumber").getValue().toString();
+                    cex = snapshot.child("expiry").getValue().toString();
+                    cvc = snapshot.child("cvc").getValue().toString();
+
+                    cardName.setText(cname);
+                    cardNumber.setText(cnum);
+                    cardEx.setText(cex);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+//itemRef.child(itemid).addValueEventListener(new ValueEventListener() {
+//@Override
+//public void onDataChange(@NonNull DataSnapshot snapshot) {
+//        if(snapshot.exists()){
+////                    for(DataSnapshot d: snapshot.getChildren()){
+//        itemNAME = snapshot.child("itemName").getValue().toString();
+//        itemC = snapshot.child("category").getValue().toString();
+//        itemP = snapshot.child("itemprice").getValue().toString();
+//        itemM = snapshot.child("manufacturer").getValue().toString();
+//
+//        //error check q
+//
+//        itemtitle.setText(itemNAME);
+//        itemcategory.setText(itemC);
+//        itemprice.setText(itemP);
+//        itemmanuf.setText(itemM);
+//
+//
+//        //}
+//
+//        }
     }
 }
+
+
+//itemRef.child(itemid).addValueEventListener(new ValueEventListener() {
+//@Override
+//public void onDataChange(@NonNull DataSnapshot snapshot) {
+//        if(snapshot.exists()){
+////                    for(DataSnapshot d: snapshot.getChildren()){
+//        itemNAME = snapshot.child("itemName").getValue().toString();
+//        itemC = snapshot.child("category").getValue().toString();
+//        itemP = snapshot.child("itemprice").getValue().toString();
+//        itemM = snapshot.child("manufacturer").getValue().toString();
+//
+//        //error check q
+//
+//        itemtitle.setText(itemNAME);
+//        itemcategory.setText(itemC);
+//        itemprice.setText(itemP);
+//        itemmanuf.setText(itemM);
+//
+//
+//        //}
+//
+//        }
