6348aac01fa3e58b1ff303af2a985735e048b753
JaymeMorrissey99 <jaymemorrissey99@gmail.com>
2022-04-26 00:06:24 +0100

    payment

--- app/src/main/java/com/example/assignment4/Payment.java
+++ app/src/main/java/com/example/assignment4/Payment.java
@@ -1,14 +1,80 @@
 package com.example.assignment4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Adaptor.MyCartAdaptor;
+import Models.Cart;
 
 public class Payment extends AppCompatActivity {
+
+    RecyclerView payRV;
+    TextView totalPrice;
+    DatabaseReference stockRef, cartRef;
+    FirebaseAuth mAuth;
+    FirebaseUser muser;
+    DatabaseReference stockref;
+
+    private MyCartAdaptor myCartAdaptor;
+    private List<Cart> cartList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        mAuth = FirebaseAuth.getInstance();
+        muser = mAuth.getCurrentUser();
+        payRV = findViewById(R.id.paymentRV);
+        totalPrice = findViewById(R.id.ttlPrice);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        payRV.setLayoutManager(linearLayoutManager);
+        //new ItemTouchHelper(itemtouch).attachToRecyclerView(cartRV);
+        cartList = new ArrayList<>();
+        myCartAdaptor = new MyCartAdaptor(this, cartList);
+        payRV.setAdapter(myCartAdaptor);
+
+        LoadCart();
+    }
+
+    private void LoadCart() {
+        String u = muser.getUid();
+        cartRef = FirebaseDatabase.getInstance().getReference().child("MyCart");
+        Query q = cartRef.orderByChild("CustomerID").equalTo(u);
+
+        q.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                cartList.clear();
+                for(DataSnapshot d: snapshot.getChildren()){
+                    Cart c = d.getValue(Cart.class);
+                    cartList.add(c);
+                }
+                myCartAdaptor.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
