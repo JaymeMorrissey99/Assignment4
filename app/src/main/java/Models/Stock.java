9bf8ee449288931134faa40676e02683b0d98c0c
JaymeMorrissey99 <jaymemorrissey99@gmail.com>
2022-04-25 01:29:48 +0100

    populate stock class

--- app/src/main/java/Models/Stock.java
+++ app/src/main/java/Models/Stock.java
@@ -1,4 +1,52 @@
 package Models;
 
 public class Stock {
+
+    String title;
+    Double price;
+    String category;
+    String image;
+
+    public Stock() {
+    }
+
+    public Stock(String title, Double price, String category, String image) {
+        this.title = title;
+        this.price = price;
+        this.category = category;
+        this.image = image;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
--- app/src/main/java/com/example/assignment4/AdminStock.java
+++ app/src/main/java/com/example/assignment4/AdminStock.java
@@ -3,18 +3,43 @@ package com.example.assignment4;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
 public class AdminStock extends AppCompatActivity {
 
     RecyclerView sRV;
     FloatingActionButton addS;
+    DatabaseReference stockRef, userRef;
+    FirebaseAuth mAuth;
+    FirebaseUser muser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_stock);
+
+        sRV = findViewById(R.id.stockrv);
+        addS = findViewById(R.id.addStock);
+
+        addS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendToAddStock();
+            }
+        });
+    }
+
+    private void sendToAddStock() {
+        //Toast.makeText(AdminStock.this, "Successful Register", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(AdminStock.this, AddStock.class);
+        startActivity(intent);
     }
 }
