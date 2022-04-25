ff97ec0bab792f38eb21c76483d4c6cd1736c87f
JaymeMorrissey99 <jaymemorrissey99@gmail.com>
2022-04-25 01:43:29 +0100

    add stock layout

--- app/src/main/java/com/example/assignment4/AddStock.java
+++ app/src/main/java/com/example/assignment4/AddStock.java
@@ -3,12 +3,26 @@ package com.example.assignment4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class AddStock extends AppCompatActivity {
+
+    ImageView iPic;
+    EditText itemtitle, itemcategory, itemprice;
+    Button addsbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_stock);
+
+        iPic = findViewById(R.id.itemPic);
+        itemtitle = findViewById(R.id.ititle);
+        itemcategory = findViewById(R.id.icategory);
+        itemprice = findViewById(R.id.iprice);
+
+        addsbtn = findViewById(R.id.additem);
     }
 }
