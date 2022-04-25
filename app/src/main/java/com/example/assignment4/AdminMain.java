82a835e687391a84aa14c23d291e04d196307b7b
JaymeMorrissey99 <jaymemorrissey99@gmail.com>
2022-04-25 01:02:07 +0100

    admin main layout

--- app/src/main/java/com/example/assignment4/AdminMain.java
+++ app/src/main/java/com/example/assignment4/AdminMain.java
@@ -6,12 +6,15 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class AdminMain extends AppCompatActivity {
+
+    Button stockbtn, customerdbtn, purchase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
