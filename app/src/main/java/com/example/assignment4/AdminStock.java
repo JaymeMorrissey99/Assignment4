e3bc9e1eca0bf2c86873b5a0e3d5c433c0042cd7
JaymeMorrissey99 <jaymemorrissey99@gmail.com>
2022-04-25 01:22:42 +0100

    Add stock layout

--- app/src/main/java/com/example/assignment4/AdminStock.java
+++ app/src/main/java/com/example/assignment4/AdminStock.java
@@ -1,10 +1,16 @@
 package com.example.assignment4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class AdminStock extends AppCompatActivity {
+
+    RecyclerView sRV;
+    FloatingActionButton addS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
